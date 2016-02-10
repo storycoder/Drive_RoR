c155a2602074376313ea74507e8d2b18a92ac7dc
Storycoder <joseph.margolis@gmail.com>
2016-02-10 09:47:50 -0500

    fave

---
+++ fave.rb
@@ -0,0 +1,13 @@
+print "What's your first name? "
+first_name = gets.chomp
+#make a line of code here that will capitalize a user's string stored in 'first_name'.
+
+print "What's your last name? "
+last_name = gets.chomp
+#make a line of code here that will capitalize a user's string stored in 'last_name'.
+
+print "Who's your favorite performer or act? "
+performer = gets.chomp
+#make a line of code here that will capitalize a user's string stored in 'performer'.
+
+print "My name #{first_name} #{last_name}. My favorite performer or act is #{performer}."
